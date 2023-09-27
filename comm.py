92e33271f8ac1879291559fa8b7cf71d3a38ee78
mmakatshu <mmakatshu@gmail.com>
2023-09-27 09:27:02 +0200

    feat: added a response feature

--- comm.py
+++ comm.py
@@ -1,2 +1,11 @@
 def communicate(conversation_starter="Hello Comm!"):
+    """
+        This is a docstring for the communicate function.
+    """
     return conversation_starter
+
+def response():
+    """
+        This is a docstring for the response function.
+    """
+    return "Hello Response!"
